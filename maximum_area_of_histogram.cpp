6075639b2b0a4103abfe64f483bd769827dc994c
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-09-09 17:20:07 +0530

    initial commit

--- maximum_area_of_histogram.cpp
+++ maximum_area_of_histogram.cpp
@@ -1,64 +1,160 @@
- vector <long long> second(long long a[], int n)
-    {
-      vector<long long> v2;
-      stack<pair<long long,int>> s;
-      for(int i=n-1;i>=0;i--){
-          if(s.size()==0){
-              v2.push_back(n);
-          }
-          else if(s.top().first<a[i]){
-              v2.push_back(s.top().second);
-          }
-          else{
-              while(s.size()!=0&&s.top().first>=a[i]){
-                  s.pop();
-              }
-              if(s.size()==0){
-                  v2.push_back(n);
-              }
-              else{
-                  v2.push_back(s.top().second);
-              }
-          }
-          s.push({a[i],i});
-      }
-      reverse(v2.begin(),v2.end());
-      return v2;
-    }
-     vector <long long> first(long long a[], int n)
-    {
-      vector<long long> v1;
-      stack<pair<long long,int>> s;
-      for(int i=0;i<n;i++){
-          if(s.size()==0){
-              v1.push_back(-1);
-          }
-          else if(s.top().first<a[i]){
-              v1.push_back(s.top().second);
-          }
-          else{
-              while(s.size()!=0&&s.top().first>=a[i]){
-                  s.pop();
-              }
-              if(s.size()==0){
-                  v1.push_back(-1);
-              }
-              else{
-                  v1.push_back(s.top().second);
-              }
-          }
-          s.push({a[i],i});
-      }
-      return v1;
-    }
-    long long getMaxArea(long long a[], int n)
-    {
-        vector<long long> v1=first(a,n);
-        vector<long long> v2=second(a,n);
-        vector<long long> v;
-        for(int i=0;i<n;i++){
-            v.push_back((v2[i]-v1[i]-1)*a[i]);
-            
-        }
-       long long x=*max_element(v.begin(), v.end());
-        return x;
+class Solution
+{
+	public:
+		int getMaxArea(vector<int> &arr, int n)
+		{
+			vector<int> left, right;
+			stack<pair<int, int>> s1, s2;
+			int pseudo_index = -1;
+			int pseudo_index1 = n;
+			for (int i = 0; i < n; i++)
+			{
+				if (s1.size() == 0)
+				{
+					left.push_back(pseudo_index);
+				}
+				else if (s1.size() > 0 && s1.top().first < arr[i])
+				{
+					left.push_back(s1.top().second);
+				}
+				else if (s1.size() > 0 && s1.top().first >= arr[i])
+				{
+					while (s1.size() > 0 && s1.top().first >= arr[i])
+					{
+						s1.pop();
+					}
+
+					if (s1.size() == 0)
+					{
+						left.push_back(pseudo_index);
+					}
+					else
+					{
+						left.push_back(s1.top().second);
+					}
+				}
+
+				s1.push({ arr[i], i });
+			}
+
+			for (int i = n - 1; i >= 0; i--)
+			{
+				if (s2.size() == 0)
+				{
+					right.push_back(pseudo_index1);
+				}
+				else if (s2.size() > 0 && s2.top().first < arr[i])
+				{
+					right.push_back(s2.top().second);
+				}
+				else if (s2.size() > 0 && s2.top().first >= arr[i])
+				{
+					while (s2.size() > 0 && s2.top().first >= arr[i])
+					{
+						s2.pop();
+					}
+
+					if (s2.size() == 0)
+					{
+						right.push_back(pseudo_index1);
+					}
+					else
+					{
+						right.push_back(s2.top().second);
+					}
+				}
+
+				s2.push({ arr[i], i });
+			}
+
+			reverse(right.begin(), right.end());
+			int m = INT_MIN;
+			for (int i = 0; i < n; i++)
+			{
+				int s = (right[i] - left[i] - 1) *arr[i];
+				m = max(m, s);	// taking max after finding area
+			}
+
+			return m;
+		}
+
+	int largestRectangleArea(vector<int> &heights)
+	{
+		int n = heights.size();
+		return getMaxArea(heights, n);
+
+	}
+};
+
+
+
+
+
+// class Solution {
+// public:
+//     int getareaofhistogram(vector<int>& heights,int n){
+//         stack<pair<int,int>> st;
+//         stack<pair<int,int>>st2;
+//         int pseudoind=-1;
+//         vector<int>nsr,nsl;
+//         vector<int>width;
+//         vector<int>area;
+//         // int n=heights.size();
+//         for(int i=0;i<n;i++)
+//         {
+//            if(st.empty()){
+//                nsl.push_back(pseudoind);   
+//            }
+//             else if(st.size()>0&&st.top().first<heights[i]){
+//                 nsl.push_back(st.top().second);
+//             } 
+//             else if(st.size()>0&&st.top().first>=heights[i]){
+//                 while(st.size()>0&&st.top().first>=heights[i]){
+//                     st.pop();
+//                 }
+//                 if(st.size()==0){
+//                     nsl.push_back(pseudoind);
+//                 }
+//                 else
+//                     nsl.push_back(st.top().second);
+//             }
+//             st.push({heights[i],i});
+//         }
+
+    
+//         for(int i=n-1;i>0;i--)
+//         {
+//             if(st2.empty()){
+//                nsr.push_back(n);   
+//            }
+//             else if(st2.size()>0&&st2.top().first<heights[i]){
+//                 nsr.push_back(st2.top().second);
+//             } 
+//             else if(st2.size()>0&&st2.top().first>=heights[i]){
+//                 while(st2.size()>0&&st2.top().first>=heights[i]){
+//                     st2.pop();
+//                 }
+//                 if(st2.size()==0){
+//                     nsr.push_back(n);
+//                 }
+//                 else
+//                     nsr.push_back(st2.top().second);
+//             }
+//             st.push({heights[i],i});
+//         }
+//     reverse(nsr.begin(),nsr.end());
+
+//     int m=INT_MIN;
+//     for(int i=0;i<n;i++){
+//         long  s=(nsr[i]-nsl[i]-1)*heights[i];
+//         m=max(m,s);
+//     }
+//     return m;
+//     }
+
+//     int largestRectangleArea(vector<int>heights)
+//     {
+//         int n=heights.size();
+//             return getareaofhistogram(heights,n);
+//     }
+// };
